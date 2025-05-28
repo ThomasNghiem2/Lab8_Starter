176d49f29329297b8f35c38ab618869c57d83146
Thomas Nghiem <tnghiem@ucsd.edu>
2025-05-28 00:01:53 -0700

    completed tasks

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab8-Starter
+
+Thomas Nghiem <br>
+
+Graceful degredation is related to service workers as service workers help contribute to graceful degredation, adding functionality and resilience to our application even when the user environment such as no connection to the internet may happen.
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,21 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if("serviceWorker" in navigator) {
+    try {
+      window.addEventListener('load', async function() {
+        const register = await navigator.serviceWorker.register("/sw.js", {
+          scope: "/",
+        })
+        if(register.active) {
+          console.log("success");
+        }
+      })
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -68,15 +83,36 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  if(localStorage.getItem('recipes') != null) {
+    return JSON.parse(localStorage.getItem('recipes'));
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let recipeArr = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  return new Promise(async(resolve, reject) => {
+    for(let i = 0; i < RECIPE_URLS.length; i++) {
+      try {
+        const url = await fetch(RECIPE_URLS[i]);
+        const recipe = await url.json();
+        recipeArr[i] = recipe;
+        if(i == RECIPE_URLS.length - 1) {
+          saveRecipesToStorage(recipeArr);
+          resolve(recipeArr);
+        }
+      } 
+      catch(error) {
+        console.error(error);
+        reject(error);
+      }
+    }
+  })
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
